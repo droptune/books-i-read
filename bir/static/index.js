904f2514055d78cf67c24816791561d9ebdbe634
Dmitriy <droptune@gmail.com>
2020-08-24 16:53:17 +0300

    Fix and tidy HTML/JS files

--- bir/static/index.js
+++ bir/static/index.js
@@ -1,28 +1,33 @@
 // Draw progress bars
-const canvases = document.getElementsByClassName("progress");
-for (let i = 0; i < canvases.length; i++) {
-  const canvas = canvases[i];
-  const ctx = canvas.getContext('2d');
+console.log("LOG");
+window.onload = () => {
+  console.log("WORKING");
+  const canvases = document.getElementsByClassName("progress");
+  for (let i = 0; i < canvases.length; i++) {
+    console.log("Drawing...");
+    const canvas = canvases[i];
+    const ctx = canvas.getContext('2d');
 
-  const currentPage = canvas.dataset.current;
-  const totalPages = canvas.dataset.total;
-  const max_width = canvas.width;
-  const height = canvas.height;
-  const progress = max_width/totalPages * currentPage;
+    const currentPage = canvas.dataset.current;
+    const totalPages = canvas.dataset.total;
+    const max_width = canvas.width;
+    const height = canvas.height;
+    const progress = max_width/totalPages * currentPage;
 
-  ctx.save();
-  ctx.fillStyle = "#10ff10";
-  ctx.fillRect(0, 0, progress, height);
-  ctx.strokeStyle = "#bfbfbf";
-  ctx.lineWidth = 2;
-  offset = ctx.lineWidth * 2;
-  ctx.strokeRect(0, 0, max_width, height);
+    ctx.save();
+    ctx.fillStyle = "#10ff10";
+    ctx.fillRect(0, 0, progress, height);
+    ctx.strokeStyle = "#bfbfbf";
+    ctx.lineWidth = 2;
+    offset = ctx.lineWidth * 2;
+    ctx.strokeRect(0, 0, max_width, height);
 
-  ctx.fillStyle = "#333";
-  ctx.font = '15px sans-serif';
-  const pagesText = currentPage + ' pages out of ' + totalPages + ' read';
-  ctx.textAlign = 'center';
-  ctx.fillText(pagesText, max_width/2, 17);
+    ctx.fillStyle = "#333";
+    ctx.font = '15px sans-serif';
+    const pagesText = currentPage + ' pages out of ' + totalPages + ' read';
+    ctx.textAlign = 'center';
+    ctx.fillText(pagesText, max_width/2, 17);
 
-  ctx.restore();
+    ctx.restore();
+  }
 };
